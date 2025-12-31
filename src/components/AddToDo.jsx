78f0753120eb79b09e76feacbcdee098632eea84
piyusharmas <piyusharmas2005@gmail.com>
2026-01-01 00:07:13 +0530

    feat: add AddForm component for adding new tasks to the To-Do list

---
+++ src/components/AddToDo.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import { useDispatch , useSelector } from 'react-redux';
+import { addTodo } from '../features/todo/todoSlice.js';
+
+
+export default function AddForm() {
+    const [task, setTask] = useState('');
+    const dispatch = useDispatch();
+
+    const submithandler = (e)=>{
+    e.preventDefault();
+    console.log(task);
+    dispatch(addTodo(task));
+    }
+
+    return (
+        <>
+            <form onSubmit={submithandler}>
+                <input type="text" 
+                placeholder="Add a new task" 
+                onChange={(e)=>setTask(e.target.value)}/>
+                <button>Add Task</button>
+            </form>
+        </>
+    );
+}
--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,9 +1,12 @@
 import {useSelector} from 'react-redux';
+import AddForm from './AddToDo';
+
 export default function ToDo() {
     const todos = useSelector((state) => state.todos);
     return (
         <div>
             <h2>To-Do</h2>
+            <AddForm />
             {todos.map((todo) => (
                 <li key={todo.id}>
                     <p>{todo.task} - {todo.isDone ? 'Done' : 'Pending'}</p>
