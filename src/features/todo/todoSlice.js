5de80f64a267ab5e4216ed2c8a0d52ea22bbc2f7
piyusharmas <piyusharmas2005@gmail.com>
2025-12-31 23:29:58 +0530

    modified reducer functions

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,26 +1,35 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice,nanoid} from '@reduxjs/toolkit';
 
 const initialState = {
-    todos: [],
+    todos: [{id:1, task: 'Sample Todo', isDone: false}],
 };
 
 export const todoSlice = createSlice({
     name: 'todo',
     initialState,
-    reducers: {
+    reducers: { // Action creators and reducers
         addTodo: (state, action) => {
-            state.todos.push(action.payload);
-        },
-        removeTodo: (state, action) => {
-            state.todos = state.todos.filter(todo => todo.id !== action.payload);
+            const newTodo = {
+                id: nanoid(),
+                task: action.payload,
+                isDone: false
+            };
+            state.todos.push(newTodo); // Immer allows us to write "mutating" logic in reducers
         },
         markAsDone: (state, action) => {
-            const todo = state.todos.find(todo => todo.id === action.payload);
-            if (todo) {
-                todo.done = true;
-            }
+            // Find the todo with the given id and mark it as done  
+            // Iterate through todos and update the isDone property
+            state.todos = state.todos.map((todo) => {
+                if (todo.id === action.payload) {
+                    todo.isDone = true;
+                }
+            });
         },
+        deleteTodo: (state, action) => {
+            // Remove the todo with the given id
+            // Filter out the todo to be deleted
+            // and return a new array without it
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+        }
     },
 }); 
-
-export const { addTodo, removeTodo, markAsDone } = todoSlice.actions;
